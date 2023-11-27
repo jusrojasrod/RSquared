5ea892b006966eaf7e3261d08853ffc064c70f3a
jusrojasrod <rebelion.96@gmail.com>
2023-11-27 10:49:45 -0500

    Create strategy.py

---
+++ RSquared/strategy.py
@@ -0,0 +1,24 @@
+import utils
+
+class strategy():
+
+    def __init__(self, start=None, end=None):
+        self.start = start
+        self.end = end
+
+        self.tickers = tickers() # define function
+
+    def etf_dates(self):
+        pass
+
+    def factors_dates(self):
+        pass
+
+    def fama_french_data():
+        pass
+
+    def momento_data():
+        pass
+
+    def etf_data():
+        pass
