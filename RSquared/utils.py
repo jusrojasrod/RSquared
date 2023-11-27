d415d5be070a4363c8a0e880dc8b2b1789daea42
jusrojasrod <rebelion.96@gmail.com>
2023-11-27 12:05:48 -0500

    Update utils.py

--- RSquared/utils.py
+++ RSquared/utils.py
@@ -5,4 +5,10 @@ def data_format(data, format="%Y%m", period="M"):
     """
     """
     data.index = pd.to_datetime(data.index, format=format).to_period(period)
-    return 
+    return data
+
+
+def percentile(serie):
+    """
+    """
+    return pd.qcut(serie, 5, labels=False)
