a9b67a141cd4393f327fb1d4108ecfe48acd2635
jusrojasrod <rebelion.96@gmail.com>
2023-12-01 15:02:26 -0500

    updated

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from RSquared.strategy import Strategy
 if __name__ == "__main__":
     run1 = Strategy()
 
-    print(run1.get_fama_french())
-    print(run1._ff_data)
+    print(run1.set_etf_data())
+    print(run1._etf_data)
