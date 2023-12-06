9b626bb620f60394b9cba8a3bd9f053937ba3dfe
jusrojasrod <rebelion.96@gmail.com>
2023-12-06 14:45:35 -0500

    Update strategy.py

--- RSquared/strategy.py
+++ RSquared/strategy.py
@@ -1,101 +1,100 @@
-import pandas as pd
+# import pandas as pd
 from datetime import date
 
 # import utils
 # import paths
 from . import sectors as s
-from . import data
+# from . import data
 
 
 class Strategy:
 
-    # TICKERS = s.ETF_sectors
+    TICKERS = s.ETF_sectors
 
-    def __init__(self, y_back=5, start=None, end=date.today(),
+    def __init__(self, lookback_period=5, start=None, end=date.today(),
                  column_name="Close"):
-        # self.column_name = column_name
+        self.column_name = column_name
+        self.lookback_period = lookback_period
 
-        # # Fama-French dates
-        # self.start_ff = start
-        # self.end_ff = end
+        # Fama-French dates
+        self.endFF_date = end
+        self.startFF_date = start
+        if start is None:
+            self.startFF_date = date(self.endFF_date.year
+                                     - self.lookback_period,
+                                     self.endFF_date.month,
+                                     self.endFF_date.day)
 
-        # # ETFs dates
-        # self.start_etf = None
-        # self.end_etf = None
-        # self.y_back = y_back
+    @property
+    def factors_end_date(self):
+        return self.endFF_date
 
-        # self._ff_data = None
+    @factors_end_date.setter
+    def factors_end_date(self, end):
+        self.endFF_date = end
 
-        # self.set_fama_french()
-        # # initialize dates
-        # self.set_factors_dates()
-        # self.set_etf_dates()
+    @property
+    def factors_start_date(self):
+        return self.start_ff
 
-        # # Initialize fama-franch and ETF data
+    @factors_start_date.setter
+    def factors_start_date(self, start):
+        self.startFF_date = start
 
-        # self.set_etf_data()
+    # def etf_dates(self):
+    #     # self.start_etf = date(self.end_ff.year - self.y_back,
+    #     #                       self.end_ff.month - 1,
+    #     #                       1)
+    #     # last_ff_date = self._ff_data.index[-1]
+    #     # self.end_etf = date(last_ff_date.year,
+    #     #                     last_ff_date.month,
+    #     #                     29)
+    #     pass
 
-    def set_etf_dates(self):
-        # self.start_etf = date(self.end_ff.year - self.y_back,
-        #                       self.end_ff.month - 1,
-        #                       1)
-        # last_ff_date = self._ff_data.index[-1]
-        # self.end_etf = date(last_ff_date.year,
-        #                     last_ff_date.month,
-        #                     29)
-        pass
+    # def set_fama_french(self):
+    #     # self._ff_data = data.famaFrenchdownload(start=self.start_ff,
+    #     #                                         end=self.end_ff)[0]/100
+    #     pass
 
-    def set_factors_dates(self):
-        # if self.start_ff is None:
-        #     self.start_ff = date(self.end_ff.year - self.y_back,
-        #                          self.end_ff.month,
-        #                          self.end_ff.day)
-        pass
+    # def get_fama_french(self):
+    #     # if self._ff_data:
+    #     #     self._ff_data = self._set_fama_french()
+    #     # return self._ff_data
+    #     pass
 
-    def set_fama_french(self):
-        # self._ff_data = data.famaFrenchdownload(start=self.start_ff,
-        #                                         end=self.end_ff)[0]/100
-        pass
+    # def set_momento_data():
+    #     pass
 
-    def get_fama_french(self):
-        # if self._ff_data:
-        #     self._ff_data = self._set_fama_french()
-        # return self._ff_data
-        pass
+    # def set_etf_data(self):
+    #     # df = data.downloadData(tickers=self.symbols,
+    #     #                        start=self.start_etf,
+    #     #                        end=self.end_etf)[self.column_name]
+    #     # return df
+    #     # self._etf_data = pd.datetime(df.index,
+    #     #                              format="%Y%m").to_period("M")
+    #     # # print(df)
+    #     pass
 
-    def set_momento_data():
-        pass
+    # def concat_factors():
+    #     pass
 
-    def set_etf_data(self):
-        # df = data.downloadData(tickers=self.symbols,
-        #                        start=self.start_etf,
-        #                        end=self.end_etf)[self.column_name]
-        # return df
-        # self._etf_data = pd.datetime(df.index,
-        #                              format="%Y%m").to_period("M")
-        # # print(df)
-        pass
+    # def returns():
+    #     pass
 
-    def concat_factors():
-        pass
+    # def excess_return():
+    #     pass
 
-    def returns():
-        pass
+    # def plot_returns():
+    #     pass
 
-    def excess_return():
-        pass
+    # def _regression():
+    #     pass
 
-    def plot_returns():
-        pass
+    # def join_reg_factors():
+    #     pass
 
-    def _regression():
-        pass
+    # def r_squared():
+    #     pass
 
-    def join_reg_factors():
-        pass
-
-    def r_squared():
-        pass
-
-    def strategy():
-        pass
+    # def strategy():
+    #     pass
